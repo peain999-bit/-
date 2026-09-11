9d1abe3558aaac777a966472cd2e5eca519c06e5
양현민 <peain999@gmail.com>
2026-09-11 14:11:20 +0900

    2주차 수업내용

---
+++ Week02/Sample02-3/src/Main.java
@@ -0,0 +1,22 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+void main() {
+    Scanner keyboard = new Scanner(System.in);
+    String name;
+    int age;
+    double height;
+    float weight;
+
+    System.out.printf("당신의 이름은 ? ");
+    name = keyboard.nextLine();
+    System.out.printf("%s님의 나이는 ? ", name);
+    age - keyboard.nextLine();
+    System.out.printf("%s님의 키는 ? ", name);
+    height - keyboard.nextLine();
+    System.out.printf("%s님의 몸무게는 ? , name");
+    weight = keyboard.nextFloat();
+
+    System.out.printf("")
+
+}
+ =
